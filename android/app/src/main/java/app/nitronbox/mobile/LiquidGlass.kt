3cf58ce77e5af28ef1a06dc77cfd913031c2c6ea
nitronnnn <bilsheefr@gmail.com>
2026-08-03 19:53:02 +0500

    Fix Compose glass layer scope

--- android/app/src/main/java/app/nitronbox/mobile/LiquidGlass.kt
+++ android/app/src/main/java/app/nitronbox/mobile/LiquidGlass.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
-import androidx.compose.foundation.layout.matchParentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
